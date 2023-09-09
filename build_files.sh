3fe52556161d4ce5719e5c53a1a18464121951b1
dhruvpatel16120 <143148503+dhruvpatel16120@users.noreply.github.com>
2023-09-09 15:59:37 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 # build_files.sh
 echo "BUILD IS START NOW !...."
 pip install -r requirements.txt
-python3.11 manage.py collectstatic
+python3.9 manage.py collectstatic
 
 echo "Python Migration is Start"
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 
 echo "BUILD IS SUCCESSFUL !!"
